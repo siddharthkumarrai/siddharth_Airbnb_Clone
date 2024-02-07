4aeb35b12709c310d7326847f0aaf9ff6becbc75
siddharthkumarrai <149219558+siddharthkumarrai@users.noreply.github.com>
2024-02-07 19:23:31 +0530

    Update app.js
    remove some data

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if(process.env.NODE_ENV != "production"){
-    require('dotenv').config();
-};
-
 const express = require("express");
 const MongoStore = require('connect-mongo');
 const app = express();
